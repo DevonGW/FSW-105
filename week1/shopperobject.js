66096e8faa150273bc18d2076cdc2fe7fdeeac1a
DevonGW <>
2020-04-13 19:37:54 -0700

    pause and practice

---
+++ week1/shopperobject.js
@@ -0,0 +1,11 @@
+var shopper = {
+    firstName = "Thomas",
+    lastName = "Jonnes",
+    age= 26,
+    groceryCart= "orange, popcorn, pasta, milk, sugar",
+    isCartFull= "false",
+    fullName = function(){
+        return this.firstNamen +" " + this.lastName;}
+    } ;
+
+    console.log(firstName+lastName);
