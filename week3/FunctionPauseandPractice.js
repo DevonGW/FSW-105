9934fa2507c9daef774ce1bed557422975933be4
DevonGW <>
2020-05-02 20:33:45 -0700

    function practice

---
+++ week3/FunctionPauseandPractice.js
@@ -0,0 +1,49 @@
+function myFunction (d1, d2){
+    return d1 * d2;
+}
+
+var result = myFunction(8,4);
+console.log(result)
+
+//part 2
+
+function largestNum (d1, d2, d3){
+    for(i = 0; i < largestNum.length; i++)
+    return  d1, d3, d3
+}
+
+var result= Math.max (3, 8, 5)
+console.log(result)
+
+//part 3
+
+function evenOrOdd(d1){
+    if(evenOrodd % 2==0){
+    console.log ("is even");
+    }
+    else if (evenOrOdd % 2==1){
+        console.log("is odd")
+    }
+    return evenOrodd 
+
+}
+
+var result = (15) %2 ==0
+    console.log(result)
+
+//part 4 comment: This was a very frustrating function, as I spent the better part of an hour trying to figure out how to do it, using if 
+//statements and also tried using a function inside of a function, and did not get anywhere. 
+
+var myNum = "1,2,3,4,5,6,7,8,9,10,11,";
+
+function isString (myNum){
+    if(myNum.length <= 22);
+    else if(myNum.length >22)
+    return myNum.concat(""),myNum
+    }   
+    var result= isString
+    console.log(result)
+
+    
+        
+    
